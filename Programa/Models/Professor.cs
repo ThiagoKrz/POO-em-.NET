8d97d3dcc86ccf8fbdbbb60126198a2313ee7cd2
ThiagoKrz <ThiagoKrz>
2023-01-23 19:31:12 -0400

    Classes e abstrações em C#

---
+++ Programa/Models/Professor.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploPOO.Models
+{
+    public class Professor : Pessoa
+    {
+        
+        public Professor(string nome) :base(nome)
+        {
+
+        }
+       public decimal Salario { get; set; }
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos de Idade, meu salário é de R${Salario}.00!");
+        } 
+    }
+}
---
+++ Programa/Program.cs
@@ -0,0 +1,38 @@
+﻿using ExemploPOO.Models;
+using ExemploPOO.Interfaces;
+
+ICalculadora calc = new Calculadora();
+Console.WriteLine(calc.Multiplicar(3, 9));
+
+
+
+
+
+
+
+
+// Computador c = new Computador();
+// Console.WriteLine(c.ToString());
+
+// Pessoa p1 = new Pessoa("Bruno Valentim");
+// Aluno a1 = new Aluno("Jefferson Caminhões");
+// a1.Apresentar();
+
+// Corrente c = new Corrente();
+
+// c.Creditar(20);
+// c.Creditar(20);
+// c.ExibirSaldo();
+
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+// c1.ExibirSaldo();
+// c1.Sacar(500);
+// c1.ExibirSaldo();
+
+// Pessoa p1 = new Pessoa();
+// Console.WriteLine("Digite o nome a ser apresentado:");
+// p1.Nome = Console.ReadLine();
+// p1.Idade = 22;
+
+// p1.Apresentar();
